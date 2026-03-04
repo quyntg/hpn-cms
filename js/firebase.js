f6f10c35edc7a7b425371e49795f43429a19da5c
quyntg <quyntg94@gmail.com>
2026-03-04 09:52:16 +0700

    fix

--- js/firebase.js
+++ js/firebase.js
@@ -3,28 +3,28 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 //live
-// const firebaseConfig = { 
-//     apiKey : "AIzaSyA-29pSNfXSScqgThENJEmkFgHMHA7tN9Q" , 
-//     authDomain : "hpn-cms.firebaseapp.com" , 
-//     databaseURL : "https://hpn-cms-default-rtdb.asia-southeast1.firebasedatabase.app" , 
-//     projectId : "hpn-cms" , 
-//     storageBucket : "hpn-cms.firebasestorage.app" , 
-//     messagingSenderId : "967690823970" , 
-//     appId : "1:967690823970:web:75f22d1a6e6aaf5f9edd7b" , 
-//     measurementId : "G-7CC65P2730" 
-// };
+const firebaseConfig = { 
+    apiKey : "AIzaSyA-29pSNfXSScqgThENJEmkFgHMHA7tN9Q" , 
+    authDomain : "hpn-cms.firebaseapp.com" , 
+    databaseURL : "https://hpn-cms-default-rtdb.asia-southeast1.firebasedatabase.app" , 
+    projectId : "hpn-cms" , 
+    storageBucket : "hpn-cms.firebasestorage.app" , 
+    messagingSenderId : "967690823970" , 
+    appId : "1:967690823970:web:75f22d1a6e6aaf5f9edd7b" , 
+    measurementId : "G-7CC65P2730" 
+};
 
 //dev
-const firebaseConfig = {
-    apiKey: "AIzaSyC8AOsuW2_x-A1dhZ7pAB-yzvvo8mCEg60",
-    authDomain: "test-cms-84180.firebaseapp.com",
-    databaseURL: "https://test-cms-84180-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "test-cms-84180",
-    storageBucket: "test-cms-84180.firebasestorage.app",
-    messagingSenderId: "690419256479",
-    appId: "1:690419256479:web:3e70dd41699f07de83a6aa",
-    measurementId: "G-STBRB8TGSY"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC8AOsuW2_x-A1dhZ7pAB-yzvvo8mCEg60",
+//     authDomain: "test-cms-84180.firebaseapp.com",
+//     databaseURL: "https://test-cms-84180-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "test-cms-84180",
+//     storageBucket: "test-cms-84180.firebasestorage.app",
+//     messagingSenderId: "690419256479",
+//     appId: "1:690419256479:web:3e70dd41699f07de83a6aa",
+//     measurementId: "G-STBRB8TGSY"
+// };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
